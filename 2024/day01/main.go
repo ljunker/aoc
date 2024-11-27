5f77ad4bf4e3924f8e196449cf5e33de73d4ba94
Lars Junker <lj@hbt.de>
2024-11-28 00:06:11 +0100

    Initial commit

---
+++ 2024/day01/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"aoc-go/util"
+	_ "embed"
+	"flag"
+	"fmt"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func init() {
+	input = strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		panic("empty input.txt file")
+	}
+}
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	if part == 1 {
+		ans := part1(input)
+		util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		fmt.Println("Output:", ans)
+	} else {
+		ans := part2(input)
+		util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		fmt.Println("Output:", ans)
+	}
+}
+
+func part1(input string) int {
+	return 0
+}
+
+func part2(input string) int {
+
+	return 0
+}
