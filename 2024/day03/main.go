6e7f3da44b13d4bb91e1b275d238c68b649126bf
Lars Junker <lj@hbt.de>
2024-12-03 08:21:20 +0100

    day03

---
+++ 2024/day03/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"aoc-go/util"
+	_ "embed"
+	"flag"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func init() {
+	input = strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		panic("empty input.txt file")
+	}
+}
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 2, "part 1 or 2")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	if part == 1 {
+		ans := part1(input)
+		fmt.Println("Output:", ans)
+		err := util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		if err != nil {
+			_ = fmt.Errorf("error running copytoclipboard %w", err)
+		}
+	} else {
+		ans := part2(input)
+		fmt.Println("Output:", ans)
+		err := util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		if err != nil {
+			_ = fmt.Errorf("error running copytoclipboard %w", err)
+		}
+	}
+}
+
+func part1(input string) int {
+	re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+	sum := 0
+	matches := re.FindAllStringSubmatch(input, -1)
+	for _, match := range matches {
+		if len(match) == 3 {
+			a, err1 := strconv.Atoi(match[1])
+			b, err2 := strconv.Atoi(match[2])
+			if err1 == nil && err2 == nil {
+				sum += a * b
+			}
+		}
+	}
+	return sum
+}
+
+func part2(input string) int {
+	re := regexp.MustCompile(`mul\((\d+),(\d+)\)|do\(\)|don't\(\)`)
+	sum := 0
+	do := true
+	matches := re.FindAllStringSubmatch(input, -1)
+	for _, match := range matches {
+		if match[0] == "don't()" {
+			do = false
+		} else if match[0] == "do()" {
+			do = true
+		} else if do {
+			a, err1 := strconv.Atoi(match[1])
+			b, err2 := strconv.Atoi(match[2])
+			if err1 == nil && err2 == nil {
+				sum += a * b
+			}
+		}
+	}
+	return sum
+}
