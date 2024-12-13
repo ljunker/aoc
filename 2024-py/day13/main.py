6bebc021a914791fa5845a126c466023019f61df
Lars Junker <lj@hbt.de>
2024-12-13 13:06:28 +0100

    2024, day 13, refactoring

--- 2024-py/day13/main.py
+++ 2024-py/day13/main.py
@@ -1,12 +1,17 @@
+import os.path
 import re
+
+from kryptikkaocutils.Timer import timer
+from kryptikkaocutils.Input import write_input_to_file
 from sympy import solve, Symbol
 
 
-def play(ax, ay, bx, by, px, py):
+def play(ax, ay, bx, by, px, py, shift=False):
     a = Symbol("a", integer=True)
     b = Symbol("b", integer=True)
-    px += 10000000000000
-    py += 10000000000000
+    if shift:
+        px += 10000000000000
+        py += 10000000000000
     roots = solve(
         [a * ax + b * bx - px, a * ay + b * by - py],
         [a, b],
@@ -17,8 +22,26 @@ def play(ax, ay, bx, by, px, py):
         return 0
 
 
-s = 0
-inputs = [[int(num) for num in re.findall(r"\d+", machine)] for machine in (open("i.txt").read().split("\n\n"))]
-for e in inputs:
-    s += play(*e)
-print(s)
+@timer
+def part1(inputs):
+    s = 0
+    for e in inputs:
+        s += play(*e)
+    print("Part1:", s)
+
+
+@timer
+def part2(inputs):
+    s = 0
+    for e in inputs:
+        s += play(*e, True)
+    print("Part2:", s)
+
+
+if __name__ == "__main__":
+    fname = "i.txt"
+    if not os.path.isfile(fname):
+        write_input_to_file(2024, 13, fname)
+    inputs = [[int(num) for num in re.findall(r"\d+", machine)] for machine in (open(fname).read().split("\n\n"))]
+    part1(inputs)
+    part2(inputs)
